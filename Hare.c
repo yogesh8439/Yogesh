be0dc9114f55aedb1998ad85255fa6ac158f51a4
yogesh8439 <ychauhan8888@gmail.com>
2024-12-28 14:33:55 +0530

    Create Hare.c

---
+++ Hare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  printf("Happy New year \n ");
+printf("let's make 2025 amazing\n");
+printf("Wishes for family and friends\n");
+printf("Wishes for whatsApp\n");
+printf("Wishes for Instagram\n");
+printf("Wishes for facebook\n");
+printf("Long Happy New Year 2025 wishes for whatsApp,Facebook and Instagram\n");
+return 0;
+}
